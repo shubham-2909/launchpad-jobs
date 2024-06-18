4a1e9a68d3ea809ceee45c523fd0c127b19e6170
Shubham Gandhi <sg9769356@gmail.com>
2024-06-18 13:14:23 +0530

    groundwork initial setup and setup db

---
+++ prisma/migrations/20240618071602_db_make/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "jobs" (
+    "id" SERIAL NOT NULL,
+    "slug" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "locationType" TEXT NOT NULL,
+    "location" TEXT,
+    "description" TEXT,
+    "salary" INTEGER NOT NULL,
+    "companyName" TEXT NOT NULL,
+    "applicationEmail" TEXT,
+    "applicationUrl" TEXT,
+    "companyLogoUrl" TEXT,
+    "approved" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "jobs_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "jobs_slug_key" ON "jobs"("slug");
